62cf0779dd12b4430033c2bb15d9c7db20160c9c
Minh Tu Nguyen <88047847+MinhTuMTN@users.noreply.github.com>
2024-10-24 07:48:28 +0700

    Update

--- test-web-app/vite.config.ts
+++ test-web-app/vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/TrainingTesting/",
+  base: "/test-web-app/",
   plugins: [react()],
 });
