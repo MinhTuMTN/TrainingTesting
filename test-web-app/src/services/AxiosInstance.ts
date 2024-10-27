56700f6ddd646f5807b3b07234aef1364f5ff6f1
Minh Tu Nguyen <88047847+MinhTuMTN@users.noreply.github.com>
2024-10-27 22:24:15 +0700

    Update endpoint

--- test-web-app/src/services/AxiosInstance.ts
+++ test-web-app/src/services/AxiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "http://tuyetvi-testing.ddns.net:8080/api",
   headers: {
     "Content-type": "application/json",
     Authorization: "Bearer " + localStorage.getItem("token"),
